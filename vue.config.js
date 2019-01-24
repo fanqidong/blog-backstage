324a4a94de1652e40f652d982622090c7074756c
fanqidong <1425434413@qq.com>
2019-01-24 11:57:22 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports={
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/Abner/dist'
-    : '/',
+    publicPath: process.env.NODE_ENV === 'production'? '/Abner/dist' : '/',
 }
